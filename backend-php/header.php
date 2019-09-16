394f6831456a72b14b9cb2825b4da2bc2ec62073
unknown <junicodefire@gmail.com>
2019-09-16 16:23:14 +0100

    backend login and signup

--- backend-php/header.php
+++ backend-php/header.php
@@ -19,20 +19,6 @@
               <li><a href="#">About</li>
               <li><a href="#">Contact Us</li>
             </ul>
-
-            <div class="header-login">
-              <form action="includes/login.php" method="post">
-                <input type="text" name="mailuid" placeholder="Username/Email..">
-                <input type="password" name="passwd" placeholder="Password..">
-                <button type="submit" name="login-submit">LOGIN</button>
-              </form>
-              <a href="signup.php">SignUp</a>
-
-              <form action="includes/logout.php" method="post">
-                <button type="submit" name="logout-submit">LOGOUT</button>
-              </form>
-
-            </div>
         </nav>
 
     </header>
